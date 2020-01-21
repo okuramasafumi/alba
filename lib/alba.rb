89f3c5d0d5bfd8ca3ddf52d8eecf4450b0b485fd
OKURA Masafumi <masafumi.o1988@gmail.com>
2020-01-21 21:48:08 +0900

    Implement simplest JSON serialization
    It simply calls `Oj.dump` or `to_json` on target.

--- lib/alba.rb
+++ lib/alba.rb
@@ -1,6 +1,30 @@
 require 'alba/version'
+require 'json'
 
+# Core module
 module Alba
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.backend=(backend)
+    @backend = backend&.to_sym
+  end
+
+  def self.backend
+    @backend
+  end
+
+  def self.serialize(object)
+    fallback = ->(resource) { resource.to_json }
+    case backend
+    when :oj
+      begin
+        require 'oj'
+        ->(resource) { Oj.dump(resource) }
+      rescue LoadError
+        fallback
+      end
+    else
+      fallback
+    end.call(object)
+  end
 end
--- test/alba_test.rb
+++ test/alba_test.rb
@@ -1,11 +1,27 @@
 require 'test_helper'
 
 class AlbaTest < Minitest::Test
+  def setup
+    Alba.backend = nil
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Alba::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_backend_returns_backend_if_set
+    Alba.backend = :oj
+    assert_equal :oj, Alba.backend
+  end
+
+  def test_it_serializes_a_hash
+    hash = {foo: 42}
+    assert_equal '{"foo":42}', Alba.serialize(hash)
+  end
+
+  def test_it_serializes_a_hash_with_oj_backend
+    hash = {foo: 42}
+    Alba.backend = :oj
+    assert_equal '{"foo":42}', Alba.serialize(hash)
   end
 end
