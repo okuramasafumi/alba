eceda2ffe7b82ab0c81dc566d4aa29f7dcdc891b
OKURA Masafumi <masafumi.o1988@gmail.com>
2020-08-11 11:37:28 +0900

    Add somes tests for Hash attribute

---
+++ test/usecases/nested_array_and_hash_test.rb
@@ -0,0 +1,52 @@
+require_relative '../test_helper'
+
+class HashAttributeTest < MiniTest::Test
+  class Foo
+    attr_reader :id, :config
+    attr_accessor :bars
+
+    def initialize(id, config)
+      @id = id
+      @config = config # Hash attribute
+      @bars = []
+    end
+  end
+
+  class Bar
+    attr_reader :data
+
+    def initialize(data)
+      @data = data # Hash attribute
+    end
+  end
+
+  class FooResource
+    include Alba::Resource
+
+    attributes :id, :config
+  end
+
+  class BarResource
+    include Alba::Resource
+
+    attributes :data
+  end
+
+  class ExtendedFooResource < FooResource
+    many :bars, resource: BarResource
+    ignoring :config
+  end
+
+  def test_it_works_with_hash_attribute
+    foo = Foo.new(1, {some_key: :some_value})
+    assert_equal('{"id":1,"config":{"some_key":"some_value"}}', FooResource.new(foo).serialize)
+  end
+
+  def test_it_works_with_many_association_with_hash
+    foo = Foo.new(1, {some_key: :some_value})
+    bar1 = Bar.new(key1: :value1)
+    bar2 = Bar.new(key2: :value2)
+    foo.bars = [bar1, bar2]
+    assert_equal('{"id":1,"bars":[{"data":{"key1":"value1"}},{"data":{"key2":"value2"}}]}', ExtendedFooResource.new(foo).serialize)
+  end
+end
