efb1d41acf938865742c8371931b0ada38053341
OKURA Masafumi <masafumi.o1988@gmail.com>
2021-04-05 22:49:26 +0900

    Add test for conditional attributes
    I accidentally failed to commit this file in the previous PR.

---
+++ test/usecases/conditional_attributes_test.rb
@@ -0,0 +1,137 @@
+require_relative '../test_helper'
+
+class ConditionalAttributesTest < MiniTest::Test
+  class User
+    attr_accessor :id, :name, :profile, :articles
+
+    def initialize(id, name)
+      @id = id
+      @name = name
+      @articles = []
+    end
+  end
+
+  class Article
+    attr_accessor :user_id, :title, :body
+
+    def initialize(user_id, title, body)
+      @user_id = user_id
+      @title = title
+      @body = body
+    end
+  end
+
+  class Profile
+    attr_accessor :user_id, :email
+
+    def initialize(user_id, email)
+      @user_id = user_id
+      @email = email
+    end
+  end
+
+  class UserResource
+    include Alba::Resource
+
+    attributes :id
+  end
+
+  class ArticleResource
+    include Alba::Resource
+
+    attributes :title
+  end
+
+  class ProfileResource
+    include Alba::Resource
+
+    attributes :email
+  end
+
+  class UserResource1 < UserResource
+    attributes :name, if: proc { |_user, name| name.size >= 5 }
+  end
+
+  class UserResource2 < UserResource
+    attribute :username, if: proc { |user| user.id == 1 } do
+      'username'
+    end
+  end
+
+  class UserResource3 < UserResource
+    one :profile, resource: ProfileResource, if: proc { |_user, profile| profile.email.end_with?('com') }
+  end
+
+  class UserResource4 < UserResource
+    many :articles, resource: ArticleResource, if: proc { |_user, articles| !articles.empty? }
+  end
+
+  class UserResource6 < UserResource
+    attributes :full_name, if: proc { |user| user.respond_to?(:full_name) }
+  end
+
+  def setup
+    @user = User.new(1, 'Masafumi OKURA')
+    profile = Profile.new(1, 'test@example.com')
+    @user.profile = profile
+    article = Article.new(1, 'Hello World!', 'Hello World!!!')
+    @user.articles << article
+  end
+
+  def test_conditional_attributes_with_if
+    assert_equal(
+      '{"id":1,"name":"Masafumi OKURA"}',
+      UserResource1.new(@user).serialize
+    )
+    user = User.new(2, 'Foo')
+    assert_equal(
+      '{"id":2}',
+      UserResource1.new(user).serialize
+    )
+  end
+
+  def test_conditional_attribute_with_if
+    assert_equal(
+      '{"id":1,"username":"username"}',
+      UserResource2.new(@user).serialize
+    )
+    user = User.new(2, 'Foo')
+    assert_equal(
+      '{"id":2}',
+      UserResource2.new(user).serialize
+    )
+  end
+
+  def test_conditional_one_with_if
+    assert_equal(
+      '{"id":1,"profile":{"email":"test@example.com"}}',
+      UserResource3.new(@user).serialize
+    )
+    user = User.new(2, 'Foo')
+    profile = Profile.new(2, 'test@example.org')
+    user.profile = profile
+    assert_equal(
+      '{"id":2}',
+      UserResource3.new(user).serialize
+    )
+  end
+
+  def test_conditional_many_with_if
+    assert_equal(
+      '{"id":1,"articles":[{"title":"Hello World!"}]}',
+      UserResource4.new(@user).serialize
+    )
+    user = User.new(2, 'Foo')
+    assert_equal(
+      '{"id":2}',
+      UserResource4.new(user).serialize
+    )
+  end
+
+  def test_conditional_attribute_with_if_with_only_one_parameter
+    assert_equal(
+      '{"id":1}',
+      UserResource6.new(@user).serialize
+    )
+  end
+end
