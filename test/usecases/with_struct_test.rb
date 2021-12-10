ecd163e0e5315136b3d5767709cb6b6ad64c9c59
OKURA Masafumi <masafumi.o1988@gmail.com>
2021-12-10 22:10:53 +0900

    [Fix] Alba now works with Struct
    `Struct` is `Enumerable`, so Alba misunderstood that it's a collection.
    Now Alba checks if it's NOT a `Struct` for being a collection.

---
+++ test/usecases/with_struct_test.rb
@@ -0,0 +1,19 @@
+require_relative '../test_helper'
+
+class WithStructTest < MiniTest::Test
+  User = Struct.new(:id, :name)
+  private_constant :User
+
+  class UserResource
+    include Alba::Resource
+
+    attributes :id, :name
+  end
+
+  def test_it_works_with_struct
+    user = User.new(1, 'test')
+    assert_equal '{"id":1,"name":"test"}', UserResource.new(user).serialize
+    user2 = User.new(2, 'test2')
+    assert_equal '[{"id":1,"name":"test"},{"id":2,"name":"test2"}]', UserResource.new([user, user2]).serialize
+  end
+end
