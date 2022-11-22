aee086bb15f72ea6be5c507821baa548407964ff
Trevor Turk <trevorturk@gmail.com>
2022-11-22 09:30:40 -0600

    nit

--- lib/alba/railtie.rb
+++ lib/alba/railtie.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 module Alba
   class Railtie < Rails::Railtie
-    # If Alba is being used in a Rails app, set the inflector by default.
-    # Uses before_initialize so the user can override in an initializer in their app.
     config.before_initialize do
       Alba.inflector = :active_support
     end
