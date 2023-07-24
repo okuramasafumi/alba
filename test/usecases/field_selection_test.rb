ab5a6ff21a90512d1272a3ccb35573e1180ac387
OKURA Masafumi <masafumi.o1988@gmail.com>
2023-07-25 00:20:32 +0900

    [Test] Add field selection test
    This is inspired by https://github.com/okuramasafumi/alba/discussions/326
    I thought this could be achieved by creating a new DSL with module
    extend so this test is here to prove it.
    I don't think we should add this to the core yet, since:
    
    1. There should be better name
    2. The selection way (nested hash) is not ideal
    
    But for some people this should work quite well and I think it's
    a good showcase how flexible Alba is :)

---
+++ test/usecases/field_selection_test.rb
@@ -0,0 +1,44 @@
+require_relative '../test_helper'
+
+class FieldSelectionTest < MiniTest::Test
+  module FieldSelection
+    def field_when_selected(*attrs, key:)
+      attrs.each do |attr|
+        attribute(attr, if: proc { params[:fields].nil? ? true : params[:fields][key].include?(attr.to_sym) }) do |object|
+          object.__send__(attr)
+        end
+      end
+    end
+  end
+
+  Bar = Struct.new(:id, :name)
+  Foo = Struct.new(:id, :name, :bar)
+
+  class FooResource
+    include Alba::Resource
+    extend FieldSelection
+
+    field_when_selected :id, :name, key: :foo
+
+    one :bar do
+      extend FieldSelection
+      field_when_selected :id, :name, key: :bar
+    end
+  end
+
+  def test_field_selection_with_params
+    foo = Foo.new(1, 'foo', Bar.new(1, 'bar'))
+    assert_equal(
+      '{"id":1,"name":"foo","bar":{"id":1,"name":"bar"}}',
+      FooResource.new(foo, params: {fields: {foo: [:id, :name], bar: [:id, :name]}}).serialize
+    )
+    assert_equal(
+      '{"id":1,"name":"foo","bar":{"id":1}}',
+      FooResource.new(foo, params: {fields: {foo: [:id, :name], bar: [:id]}}).serialize
+    )
+    assert_equal(
+      '{"id":1,"name":"foo","bar":{"id":1,"name":"bar"}}',
+      FooResource.new(foo).serialize
+    )
+  end
+end
