f55259f1a618ba914b48723991dafbe1062dbdba
OKURA Masafumi <masafumi.o1988@gmail.com>
2023-12-28 18:43:17 +0900

    [CI] Use branch coverage in CI
    Let's see what happens...

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,15 +4,11 @@ require 'simplecov-cobertura'
 
 if ENV['CI']
   SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
-  SimpleCov.start do
-    add_filter '/test/'
-  end
-else
-  SimpleCov.start do
-    add_filter '/test/'
-    enable_coverage :branch
-    primary_coverage :branch
-  end
+end
+SimpleCov.start do
+  add_filter '/test/'
+  enable_coverage :branch
+  primary_coverage :branch
 end
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
