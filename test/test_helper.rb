0a20ec1dc6cc382127c48e85add94e74e1fb9e96
OKURA Masafumi <masafumi.o1988@gmail.com>
2024-11-29 23:42:03 +0900

    [Test] Add `with_inflector` helper for test
    I'm not sure if it's the right way, but it works.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,3 +15,13 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'alba'
 
 require 'minitest/autorun'
+
+class Minitest::Test # rubocop:disable Style/ClassAndModuleChildren
+  def with_inflector(inflector = :active_support)
+    original_inflector = Alba.inflector
+    Alba.inflector = inflector
+    yield
+  ensure
+    Alba.inflector = original_inflector
+  end
+end
